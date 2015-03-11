0d4fd4608d457794e636143e1959725d8910f968
Juan <yurak.sisa.dream@gmail.com>
2015-03-11 16:46:36 +0100

    json version

--- lib/ysd_md_booking_line_resource.rb
+++ lib/ysd_md_booking_line_resource.rb
@@ -14,5 +14,31 @@ module BookingDataSystem
      belongs_to :booking_line, 'BookingLine', :child_key => [:booking_line_id]
      belongs_to :booking_item, 'Yito::Model::Booking::BookingItem', 
        :child_key => [:booking_item_reference], :parent_key => [:reference], :required => false
+
+     def item_id
+       booking_line.item_id
+     end
+
+     def item_description
+       booking_line.item_description
+     end
+
+     #
+     # Exporting to json
+     #
+     def as_json(options={})
+
+       if options.has_key?(:only)
+         super(options)
+       else
+         relationships = options[:relationships] || {}
+         methods = options[:methods] || []
+         methods << :item_id
+         methods << :item_description
+         super(options.merge({:relationships => relationships, :methods => methods}))
+       end
+
+     end
+
   end
 end
