8bf977a281ab39b8729911187f47b9d6a8a93a94
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2016-01-28 09:23:02 +0100

    hourly occupation

--- lib/yito/model/booking/ysd_md_booking_queries.rb
+++ lib/yito/model/booking/ysd_md_booking_queries.rb
@@ -28,10 +28,11 @@ module Yito
                FROM bookds_bookings_lines as l
                JOIN bookds_bookings as b on b.id = l.booking_id
                JOIN bookds_categories as c on c.code = l.item_id
-               WHERE (b.date_from <= '#{from}' and b.date_to >= '#{from}') or 
+               WHERE ((b.date_from <= '#{from}' and b.date_to >= '#{from}') or 
                    (b.date_from <= '#{to}' and b.date_to >= '#{to}') or 
                    (b.date_from = '#{from}' and b.date_to = '#{to}') or
-                   (b.date_from >= '#{from}' and b.date_to <= '#{to}')
+                   (b.date_from >= '#{from}' and b.date_to <= '#{to}')) and
+                   b.status <> 4
                GROUP BY l.item_id, c.stock
             QUERY
 
@@ -39,8 +40,90 @@ module Yito
 
           end
 
+          # Get the daily percentage occupation in a period of time 
+          #
+          def daily_occupation(from, to)
+
+          end
+          
+          # Get the hourly percentage occupation in a day
+          #
+          def hourly_occupation(day)
+            
+            categories = ::Yito::Model::Booking::BookingCategory.all(fields: [:code, :stock])
+            
+            stocks = categories.inject({}) do |result, item|
+                       result.store(item.code, item.stock)
+                       result
+                     end
+
+            scheduler_start_time = Time.parse(SystemConfiguration::Variable.get_value('booking.scheduler_start_time', '07:00'))
+            scheduler_end_time = Time.parse(SystemConfiguration::Variable.get_value('booking.scheduler_finish_time', '23:30'))
+
+            scheduler = categories.inject({}) do |result, item|
+                          hours_hash = {} 
+                          (0..23).each do |hour|
+                            [0,30].each do |minute|
+                              time = "#{hour.to_s.rjust(2,'00')}:#{minute.to_s.rjust(2,'00')}"
+                              hours_hash.store(time, 0) if Time.parse(time) >= scheduler_start_time and Time.parse(time) <= scheduler_end_time
+                            end
+                          end
+                          result.store(item.code, hours_hash)
+                          result
+                        end
+
+            query = <<-QUERY
+               select bl.item_id, bl.quantity, b.date_from, b.time_from, b.date_to, b.time_to
+               from bookds_bookings_lines bl join bookds_bookings b on bl.booking_id = b.id 
+               where date_from >= '#{day}' and date_from < '#{day+1}' and b.status <> 4
+               order by bl.item_id, date_from
+            QUERY
+
+            last_time = SystemConfiguration::Variable.get_value('booking.scheduler_finish_time')
+
+            occupation = repository.adapter.select(query)
+
+            occupation.each do |item|
+              hours = hour_array(item.time_from, item.time_to, 0.5, ['00','30'], last_time)
+              hours.each do |hour|
+                scheduler[item.item_id][hour] += item.quantity
+              end
+            end
+
+            scheduler.each do |key, value|  
+              value.each do |hour, occupation| 
+                scheduler[key][hour] = "#{scheduler[key][hour]}/#{stocks[key]}"
+              end
+            end            
+
+            scheduler
+
+          end 
+
           private
     
+          def hour_array(time_from, time_to, step, steps, last_time)
+            first = time_from
+            last = time_to == last_time ? last_time : time_to.split(':').last == '00' ? "#{time_to.split(':').first.to_i-1}:30" : "#{time_to.split(':').first}:00"
+
+            duration = ((Time.parse(time_to)-Time.parse(time_from))/3600)
+            duration += step if time_to == last_time
+
+            hour = first.split(':').first.to_i
+            index = steps.index(first.split(':').last) + 1
+            index = 0 if index > steps.size-1
+
+            result = [first]
+
+            (1..duration*(1/step).to_i-1).each do |item|    
+              hour = hour + 1 if steps[index] == '00'
+              result << "#{hour.to_s.rjust(2,'00')}:#{steps[index]}"
+              index = index + 1
+              index = 0 if index > steps.size-1
+            end
+            return result
+          end
+
           def query_strategy
       
             @query_strategy ||= 
