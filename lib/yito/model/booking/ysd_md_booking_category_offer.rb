7e4e4a17bc287fd3dd1c44730c41173bdf9c9a51
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2018-04-20 14:51:48 +0200

    offers date rules

--- lib/yito/model/booking/ysd_md_booking_category_offer.rb
+++ lib/yito/model/booking/ysd_md_booking_category_offer.rb
@@ -13,6 +13,7 @@ module Yito
         storage_names[:default] = 'bookds_category_offers'
 
         property :id, Serial
+        property :reservation_dates_rule, Enum[:from_included, :all_included], default: :from_included
         belongs_to :discount, 'Yito::Model::Rates::Discount', child_key: [:discont_id], parent_key: [:id]
         has n, :offer_booking_categories, 'BookingCategoryOfferCategory',
                :child_key => [:booking_category_offer_id, :booking_category_code], :parent_key => [:id]
@@ -23,9 +24,26 @@ module Yito
         #
         def self.search_offer(booking_category_code, date_from, date_to)
 
-          offer = BookingCategoryOffer.first(conditions: {'offer_booking_categories.booking_category_code'.to_sym => booking_category_code,
-                                                          'discount.source_date_from'.to_sym.lte => date_from,
-                                                          'discount.source_date_to'.to_sym.gte => date_to})
+
+          conditions = Conditions::JoinComparison.new('$or',
+               [Conditions::JoinComparison.new('$and',
+                   [Conditions::Comparison.new('offer_booking_categories.booking_category_code','$eq', booking_category_code),
+                              Conditions::Comparison.new('discount.source_date_from','$lte', date_from),
+                              Conditions::Comparison.new('discount.source_date_to','$gte', date_to),
+                              Conditions::Comparison.new('reservation_dates_rule','$eq', :all_included)
+                             ]),
+                         Conditions::JoinComparison.new('$and',
+                   [Conditions::Comparison.new('offer_booking_categories.booking_category_code','$eq', booking_category_code),
+                             Conditions::Comparison.new('discount.source_date_from','$lte', date_from),
+                             Conditions::Comparison.new('discount.source_date_to','$gte', date_from),
+                             Conditions::Comparison.new('reservation_dates_rule','$eq', :from_included)
+                             ])
+               ])
+
+          p "conditions:#{booking_category_code}"
+
+          offer = conditions.build_datamapper(BookingCategoryOffer).first
+
           if offer
             return offer.discount
           else
